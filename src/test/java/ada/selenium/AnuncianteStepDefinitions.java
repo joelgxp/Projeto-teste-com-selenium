a5f406809c347a48b8fae3c05133ce7f0173c524
Joel <joelvieirasouza@gmail.com>
2023-04-16 21:49:36 -0300

    adição de 3 cenarios de testes

--- src/test/java/ada/selenium/AnuncianteStepDefinitions.java
+++ src/test/java/ada/selenium/AnuncianteStepDefinitions.java
@@ -1,5 +1,6 @@
 package ada.selenium;
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
@@ -8,7 +9,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class AnuncianteStepDefinitions {
     WebDriver driver;
@@ -20,6 +26,10 @@ public class AnuncianteStepDefinitions {
         options.addArguments("--remote-allow-origins=*");
         driver = WebDriverManager.chromedriver().capabilities(options).create();
     }
+//    @After
+//    public void after() {
+//        driver.quit();
+//    }
 
     @Dado("que usuario esta na homepage")
     public void que_usuario_esta_na_homepage() {
@@ -33,6 +43,40 @@ public class AnuncianteStepDefinitions {
     public void deverá_ser_redirecionado_para_pagina_do_anunciante() {
         Assertions.assertEquals("https://www.airbnb.com.br/host/homes", driver.getCurrentUrl());
     }
+
+    @Dado("que na tela de anunciantes")
+    public void que_na_tela_de_anunciantes() {
+        driver.get("http://www.airbnb.com.br/host/homes");
+    }
+    @Quando("clicar em simular o valor de hospedagens")
+    public void clicar_em_simular_o_valor_de_hospedagens() {
+        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.elementToBeClickable(By.className("_wwvtb8")));
+        driver.findElement(By.className("_wwvtb8")).click();
+    }
+    @Entao("deverá ter os campos localização, tipo de espaço, quantidades de quartos")
+    public void deverá_ter_os_campos_localização_tipo_de_espaço_quantidades_de_quartos() {
+        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[text()=\"Endereço ou região\"]")));
+        WebElement element = driver.findElement(By.xpath("//h2[text()=\"Endereço ou região\"]"));
+        Assertions.assertEquals("Endereço ou região", element.getText());
+
+    }
+
+    @Dado("qua na tela do anunciante")
+    public void qua_na_tela_do_anunciante() {
+        driver.get("http://www.airbnb.com.br/host/homes");
+        //driver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000));
+    }
+    @Quando("clicar no botão Anuncio Facil AirBnb")
+    public void clicar_no_botão_anuncio_facil_air_bnb() {
+        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.elementToBeClickable(By.cssSelector("._ug2ysd9")));
+        driver.findElement(By.cssSelector("._ug2ysd9")).click();
+    }
+    @Entao("devera abrir um modal Entrar ou cadastrar-se")
+    public void devera_abrir_um_modal_entrar_ou_cadastrar_se() {
+        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("._u72b34")));
+        Assertions.assertEquals("Bem-vindo ao Airbnb", driver.findElement(By.cssSelector("._u72b34")).getText());
+    }
+
 
 
 }
--- src/test/resources/ada/selenium/anunciante.feature
+++ src/test/resources/ada/selenium/anunciante.feature
@@ -4,12 +4,20 @@
                   Para divulgar um espaço próprio
 
 
-                  r1: Deve ter um botão "anuncie seu espaço" na tela principal que redirecione para uma outra tela com informações sobre outros anunciantes
-                  r2: Na tela de anunciantes deve ter uma simulação de valor da hospedagem com critérios como: localização, tipo de quarto e quantidade de hospedes
-
-
-
-    Cenario: valida se botao "anuncie seu espaço" redireciona para pagina correta
+    Cenario: validar se botao "anuncie seu espaço" redireciona para pagina correta
       Dado que usuario esta na homepage
       Quando clicar sobre o botao anuncie seu espaco
       Entao deverá ser redirecionado para pagina do anunciante
+
+    Cenario: Na tela de anunciantes deve ter uma simulação de valor da hospedagem com critérios como:
+    localização, tipo de quarto e quantidade de hospedes
+      Dado que na tela de anunciantes
+      Quando clicar em simular o valor de hospedagens
+      Entao deverá ter os campos localização, tipo de espaço, quantidades de quartos
+
+    Cenario: validar se o botão "Anuncio Facil AirBnb" na tela do anunciante abre um modal contendo
+    campos para realizar login ou se cadastrar
+      Dado qua na tela do anunciante
+      Quando clicar no botão Anuncio Facil AirBnb
+      Entao devera abrir um modal Entrar ou cadastrar-se
+
